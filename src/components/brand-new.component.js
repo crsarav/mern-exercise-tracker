f323f330f5dd53c346bbecde9cb576e717cbe8e6
Saravanan Palanivelu <cr.sarav@gmail.com>
2019-10-21 15:01:17 -0400

    new changes today in brand-new component and navbar component

---
+++ src/components/brand-new.component.js
@@ -0,0 +1,1 @@
+// this is brand new component added to project 
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+// made some changes here 
 export default class Navbar extends Component{
     
     render(){
