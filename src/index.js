60c3f2c0c590865ae89e249b65813ff2258ef36d
Saravanan Palanivelu <cr.sarav@gmail.com>
2019-10-21 22:42:36 -0400

    updating index.js after remote master

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// this is added after we have successfully set up remote master
 ReactDOM.render(<App />, document.getElementById('root'));
